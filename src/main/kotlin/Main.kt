64c222d882a69c956c7460f0af5c85d6facb55d0
Narges-Nagy <nargesnagy21@gmail.com>
2025-04-04 18:40:13 +0200

    add test cases to test sudoku validation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,3 +6,8 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 }
+
+
+fun isValidSudoku(list: List<Any>) : Boolean{
+ return false
+}
---
+++ src/main/kotlin/Test.kt
@@ -0,0 +1,225 @@
+
+
+
+fun main(){
+
+    test(
+        name = "when a sudoku is valid return true",
+        isValidSudoku(
+            listOf(
+                listOf(1,2,3),
+                listOf(4,5,6),
+                listOf(7,8,9)
+            )
+        ),
+        correctResult = true
+    )
+
+
+    test(
+        name = "when a sudoku is valid return true",
+        isValidSudoku(
+            listOf(
+                listOf(1,2,3),
+                listOf('-',5,6),
+                listOf(7,8,'-')
+            )
+        ),
+        correctResult = true
+    )
+
+    test(
+        name = "when a sudoku is not valid return false",
+        isValidSudoku(
+            listOf(
+                listOf(1,2,3),
+                listOf(4,5,6),
+                listOf(1,8,9)
+            )
+        ),
+        correctResult = false
+    )
+
+    test(
+        name = "when a sudoku is not valid return false",
+        isValidSudoku(
+            listOf(
+                listOf(1,2,3),
+                listOf(4,5,6),
+                listOf(7,8,6)
+            )
+        ),
+        correctResult = false
+    )
+
+    test(
+        name = "when a sudoku is valid return true",
+        isValidSudoku(
+            listOf(
+                listOf(3,2,4,1),
+                listOf(2,3,1,4),
+                listOf(1,4,2,3),
+                listOf(4,1,3,2)
+            )
+        ),
+        correctResult = true
+    )
+
+    test(
+        name = "when a sudoku is not valid return false",
+        isValidSudoku(
+            listOf(
+                listOf(3,2,4,1),
+                listOf(2,3,1,4),
+                listOf(1,4,2,3),
+                listOf(1,4,3,2)
+            )
+        ),
+        correctResult = false
+    )
+
+    test(
+        name = "when a sudoku is not valid return false",
+        isValidSudoku(
+            listOf(
+                listOf(3,2,4,1),
+                listOf(2,3,1,4),
+                listOf(1,4,2,3),
+                listOf(4,1,2,3)
+            )
+        ),
+        correctResult = false
+    )
+
+    test(
+        name = "when a sudoku is valid return true",
+        isValidSudoku(
+            listOf(
+                listOf(6,1,4,3,2,5),
+                listOf(1,3,2,6,5,4),
+                listOf(4,6,3,5,1,2),
+                listOf(2,4,5,1,3,6),
+                listOf(3,5,6,2,4,1),
+                listOf(5,2,1,4,6,3)
+            )
+        ),
+        correctResult = true
+    )
+
+    test(
+        name = "when a sudoku is not valid return false",
+        isValidSudoku(
+            listOf(
+                listOf(6,1,4,3,2,5),
+                listOf(1,3,2,6,5,4),
+                listOf(4,6,3,5,1,2),
+                listOf(2,4,5,1,3,6),
+                listOf(3,5,6,2,4,1),
+                listOf(5,1,2,4,6,3)
+            )
+        ),
+        correctResult = false
+    )
+
+
+    test(
+        name = "when a sudoku is valid return true",
+        isValidSudoku(
+            listOf(
+                listOf(5,3,4,6,7,8,9,1,2),
+                listOf(6,7,2,1,9,5,3,4,8),
+                listOf(1,9,8,3,4,2,5,6,7),
+                listOf(8,5,9,7,6,1,4,2,3),
+                listOf(4,2,6,8,5,3,7,9,1),
+                listOf(7,1,3,9,2,4,8,5,6),
+                listOf(9,6,1,5,3,7,2,8,4),
+                listOf(2,8,7,4,1,9,6,3,5),
+                listOf(3,4,5,2,8,6,1,7,9),
+            )
+        ),
+        correctResult = true
+    )
+
+
+    test(
+        name = "when a sudoku is not valid return false",
+        isValidSudoku(
+
+            listOf(
+                listOf(5, 3, 4, 6, 7, 8, 9, 1, 2),
+                listOf(6, 7, 2, 1, 9, 5, 3, 4, 8),
+                listOf(1, 9, 8, 3, 4, 2, 5, 6, 7),
+                listOf(8, 5, 9, 7, 6, 1, 4, 2, 3),
+                listOf(5, 2, 6, 8, 5, 3, 7, 9, 1),
+                listOf(7, 1, 3, 9, 2, 4, 8, 5, 6),
+                listOf(9, 6, 1, 5, 3, 7, 2, 8, 4),
+                listOf(2, 8, 7, 4, 1, 9, 6, 3, 5),
+                listOf(3, 4, 5, 2, 8, 6, 1, 7, 9)
+            )
+        ),
+        correctResult = false
+    )
+
+    test(
+        name = "when a sudoku is valid return true",
+        isValidSudoku(
+
+            listOf(  // wrong sudoku
+                listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F', 'G'),
+                listOf('B', 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'C', 'D', 'E', 'F', 'G'),
+                listOf('C', 'B', 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'D', 'E', 'F', 'G'),
+                listOf('D', 'C', 'B', 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'E', 'F', 'G'),
+                listOf('E', 'D', 'C', 'B', 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'F', 'G'),
+                listOf('F', 'E', 'D', 'C', 'B', 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'G'),
+                listOf('G', 'F', 'E', 'D', 'C', 'B', 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A'),
+                listOf(1, 'G', 'F', 'E', 'D', 'C', 'B', 2, 3, 4, 5, 6, 7, 8, 9, 'A'),
+                listOf(2, 1, 'G', 'F', 'E', 'D', 'C', 'B', 3, 4, 5, 6, 7, 8, 9, 'A'),
+                listOf(3, 2, 1, 'G', 'F', 'E', 'D', 'C', 'B', 4, 5, 6, 7, 8, 9, 'A'),
+                listOf(4, 3, 2, 1, 'G', 'F', 'E', 'D', 'C', 'B', 5, 6, 7, 8, 9, 'A'),
+                listOf(5, 4, 3, 2, 1, 'G', 'F', 'E', 'D', 'C', 'B', 6, 7, 8, 9, 'A'),
+                listOf(6, 5, 4, 3, 2, 1, 'G', 'F', 'E', 'D', 'C', 'B', 7, 8, 9, 'A'),
+                listOf(7, 6, 5, 4, 3, 2, 1, 'G', 'F', 'E', 'D', 'C', 'B', 8, 9, 'A'),
+                listOf(8, 7, 6, 5, 4, 3, 2, 1, 'G', 'F', 'E', 'D', 'C', 'B', 9, 'A'),
+                listOf(9, 8, 7, 6, 5, 4, 3, 2, 1, 'G', 'F', 'E', 'D', 'C', 'B', 'A')
+            )
+        ),
+        correctResult = true
+    )
+
+    test(
+        name = "when a sudoku is valid return false",
+        isValidSudoku(
+
+            listOf(
+                listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F', 'G'),
+                listOf('B', 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'C', 'D', 'E', 'F', 'G'),
+                listOf('C', 'B', 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'D', 'E', 'F', 'G'),
+                listOf('D', 'C', 'B', 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'E', 'F', 'G'),
+                listOf('E', 'D', 'C', 'B', 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'F', 'G'),
+                listOf('F', 'E', 'D', 'C', 'B', 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'G'),
+                listOf('G', 'F', 'E', 'D', 'C', 'B', 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A'),
+                listOf(1, 'G', 'F', 'E', 'D', 'C', 'B', 2, 3, 4, 5, 6, 7, 8, 9, 'A'),
+                listOf(2, 1, 'G', 'F', 'E', 'D', 'C', 'B', 3, 4, 5, 6, 7, 8, 9, 'A'),
+                listOf(3, 2, 1, 'G', 'F', 'E', 'D', 'C', 'B', 4, 5, 6, 7, 8, 9, 'A'),
+                listOf(4, 3, 2, 1, 'G', 'F', 'E', 'D', 'C', 'B', 5, 6, 7, 8, 9, 'A'),
+                listOf(5, 4, 3, 2, 1, 'G', 'F', 'E', 'D', 'C', 'B', 6, 7, 8, 9, 'A'),
+                listOf(6, 5, 4, 3, 2, 1, 'G', 'F', 'E', 'D', 'C', 'B', 7, 8, 9, 'A'),
+                listOf(7, 6, 5, 4, 3, 2, 1, 'G', 'F', 'E', 'D', 'C', 'B', 8, 9, 'A'),
+                listOf(8, 7, 6, 5, 4, 3, 2, 1, 'G', 'F', 'E', 'D', 'C', 'B', 9, 'A'),
+                listOf(9, 8, 7, 6, 5, 4, 3, 2, 1, 'G', 'F', 'E', 'D', 'C', 'B', 'A')
+            )
+        ),
+        correctResult = false
+    )
+
+
+}
+
+fun test(name: String, result: Boolean, correctResult: Boolean){
+    if(result == correctResult){
+        println("Success $name $result")
+        return
+    }
+    println("Fail $name $result")
+}
+
